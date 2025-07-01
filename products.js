ce82753c680e838cc2f58c2c406856ba115ff688
3moomar <egytop1x@gmail.com>
2025-07-01 23:49:35 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: "1",
+    name: "هاتف ذكي",
+    price: 1500,
+    image: "images/phone.jpg"
+  },
+  {
+    id: "2",
+    name: "لابتوب",
+    price: 4500,
+    image: "images/laptop.jpg"
+  },
+  {
+    id: "3",
+    name: "سماعات بلوتوث",
+    price: 300,
+    image: "images/headphones.jpg"
+  }
+];
